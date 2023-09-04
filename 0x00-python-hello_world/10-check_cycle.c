f9d7d4b71bf37f093c0c11058b83c386186d3a17
put username-2aaaaaaaaaadsgagagagagregeaggit config --global user.name put <basem_elgalfy@yahoo.com>
2023-09-04 15:19:29 -0700

    turtle and hare

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * check_cycle - checks to see if a cycle exists
+  * @list: linked list
+  * Return: 0 if no cylcle , 1 if ther is a cycle
+  */
+
+int check_cycle(listint_t *list)
+{
+	listint_t p1 , p2;
+
+	if (!list || !(list->next))
+		return (0);
+
+	p1 = list->next;
+	p2 = list->next->next;
+
+	while (p1 && p2 && p2->next)
+	{
+		if (p1 == p2)
+			return (1);
+		p1 = p1->next;
+		p2 = p2->next->next;
+	}
+	return (0);
+}
