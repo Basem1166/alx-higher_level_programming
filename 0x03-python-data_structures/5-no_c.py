7f378897f4845014eff26f0aca75431e0971bc8d
Basem Elgalfy <basem_elgalfy@yahoo.com>
2023-09-18 11:10:43 +0300

    !=

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for letter in my_string:
-        if letter is not 'c' and letter is not 'C':
+        if letter != 'c' and letter != 'C':
             new_string += letter
     return new_string
