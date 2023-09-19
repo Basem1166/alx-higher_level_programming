601fa3c210f9253812a0e0dc31bec3de9086d77a
Basem Elgalfy <basem_elgalfy@yahoo.com>
2023-09-19 16:56:52 +0300

    extra tasks

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return (max(a_dictionary) if a_dictionary else None)
+    return (a_dictionary.keys(max(a_dictionary.values())) if a_dictionary else None)
+mydic={"John": 12, "Alex": 8, "Bob": 14, "Mike": 14, "Molly": 16}
+print(best_score(mydic))
