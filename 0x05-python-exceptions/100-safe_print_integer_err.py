82e29f039c4dcaf0271f17b6a75e3cca70c8e6eb
Basem Elgalfy <basem_elgalfy@yahoo.com>
2023-09-26 01:35:15 +0300

    100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as inst:
+        print("Exception: {}".format(inst.args[0]), file = sys.stderr)
