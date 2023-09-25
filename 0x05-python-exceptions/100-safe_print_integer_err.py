c1459a07b46ea15550a7911fd9ed584cb9dbb0ca
Basem Elgalfy <basem_elgalfy@yahoo.com>
2023-09-26 01:36:22 +0300

    return False

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,3 +6,4 @@ def safe_print_integer_err(value):
         return True
     except (ValueError, TypeError) as inst:
         print("Exception: {}".format(inst.args[0]), file = sys.stderr)
+        return False
